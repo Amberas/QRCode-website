af3f11fe6887383577560d58b12521bd491ab834
Amberas <amber.aspeslagh@student.howest.be>
2022-01-30 22:04:08 +0100

    fix url id

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 {
 
-    const id = location.pathname.split('/')[2];
+    const url = new URL(window.location.href);
+    const id = url.searchParams.get("id");
 
     const init = () => {
         new QRCode(document.getElementById("qrcode"), `https://qr-code-six.vercel.app/?id=${id}`);
